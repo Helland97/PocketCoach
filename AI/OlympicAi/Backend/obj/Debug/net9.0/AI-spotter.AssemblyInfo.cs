b1841659277aefb27703931be26d404046769a5c
Christopher Helland <Christopher.helland@hotmail.com>
2025-12-08 14:09:24 +0100

    prepping new branch for exploration

--- AI/OlympicAi/Backend/obj/Debug/net9.0/AI-spotter.AssemblyInfo.cs
+++ AI/OlympicAi/Backend/obj/Debug/net9.0/AI-spotter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AI-spotter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80f5a30ad4fe2018455f93c4040b792b4368205c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+73ddb4cbcdf8f2099e8e294732bf817817b26469")]
 [assembly: System.Reflection.AssemblyProductAttribute("AI-spotter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AI-spotter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
